23d9c8e0fc8b5e3af4e204bbad4fa8e8f2383901
Gustavo E. Cruz <64294820+Cr00zz@users.noreply.github.com>
2022-05-06 09:09:04 -0300

    Create clayton_braia.py

---
+++ Projeto/clayton_braia.py
@@ -0,0 +1,14 @@
+import threading
+
+totalsum = 0
+
+def taylor(target):
+    soma = totalsum + (1/int(target))
+    return soma
+
+param = int(input("Insira um valor para calcular sua serie de taylor: "))
+for i in range(1,param+1):
+    soma_thread = threading.Thread(target = taylor, args=(i,))
+    soma_thread.start()
+    totalsum = taylor(i)
+print(totalsum)

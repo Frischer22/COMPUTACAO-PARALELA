d6502e6bcf52f8375d4ecd91318be048eff34d4c
Gustavo E. Cruz <64294820+Cr00zz@users.noreply.github.com>
2022-05-06 08:51:35 -0300

    Update clayton.py

--- Projeto/clayton.py
+++ Projeto/clayton.py
@@ -1,6 +1,15 @@
-param = int(input("Insira um valor para calcular sua serie de taylor: "))
-soma = 1
-for i in range(2,param+1):
-  soma = soma + (1/i)
+import threading
 
-print(soma)
+totalsum = 0
+
+def taylor(target):
+    soma = totalsum + (1/int(target))
+    return soma
+
+param = int(input("Insira um valor para calcular sua serie de taylor: "))
+for i in range(1,param+1):
+    totalsum = taylor(i)
+print(totalsum)
+
+soma_thread = threading.Thread(target = taylor, args=(i,))
+soma_thread.start()

0522efb0484bf9d03827449346ceabc0e62555a0
Gustavo E. Cruz <64294820+Cr00zz@users.noreply.github.com>
2022-05-13 09:05:36 -0300

    Update clayton rapido.py

--- Projeto/clayton rapido.py
+++ Projeto/clayton rapido.py
@@ -1,10 +1,14 @@
 from concurrent.futures import ThreadPoolExecutor, as_completed
+import asyncio
+
+num_threads = 4
+pool = ThreadPoolExecutor(num_threads)
+sema = asyncio.Semaphore(num_threads)
 
 def taylor(param,soma):
     soma += (1/param)
     return soma
 
-pool = ThreadPoolExecutor(4)
 
 futures = []
 
@@ -12,11 +16,11 @@ parametro = 1000
 
 soma = 0
 for i in range(1,parametro+1):
-    futures.append(pool.submit(taylor,i,soma))
+    futures.append(pool.submit(taylor, sema,i,soma))
 
 
 for i in as_completed(futures):
-    soma += (i.result())
+    async with sema:
+        soma += (i.result())
 
 print("Soma = ",soma)
-
